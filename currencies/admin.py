72f8ddda82bca85f0515e3e214c795ff32afc721
Nimrod S. Kerrett <unclezzzen@gmail.com>
2012-07-11 18:09:23 +0700

    Added admin for ForeignCurrency
    Handy because list_display shows current rates

--- currencies/admin.py
+++ currencies/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from currencies.models import BaseCurrency,ForeignCurrency
 
+def currency_rate(obj):
+    return obj.rate
+currency_rate.short_description = 'Rate'
+
+def currencies(obj):
+    return unicode(obj)
+currencies.short_description = 'Currencies'
+
+## ForeignCurrency
+class ForeignCurrencyAdmin(admin.ModelAdmin):
+    list_display = (currencies,'invert_rate',currency_rate)
+    list_editable = ('invert_rate',)
+    def save_model(self, request, obj, form, change):
+        obj.invalidate()
+admin.site.register(ForeignCurrency,ForeignCurrencyAdmin)
+
 ## ForeignCurrency (inline)
 class ForeignCurrencyInline(admin.TabularInline):
     model = ForeignCurrency
